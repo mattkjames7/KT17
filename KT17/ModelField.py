8e5fd31ab44c9c800a8c0922dde4f9f98d0a7588
mattkjames7 <mattkjames7@gmail.com>
2018-11-26 22:55:18 +0000

    slight change

--- KT17/ModelField.py
+++ KT17/ModelField.py
@@ -20,9 +20,11 @@ def ModelField(x, y, z, Params=[1.42,7.37,2.16]):
 		if shP[1] == _n:
 			Par = Par.T
 			shP = Par.shape
-		
+		_nP = np.int32(shP[1]) # number of parameters for each trace
+	else:
+		_nP = np.int32(shP[0])
 	_Plen = np.int32(np.size(Par)) # total number of parameters
-	_nP = np.int32(shP[1]) # number of parameters for each trace
+	
 	_Params = Par.flatten().astype('float64')
 	
 	_Ckt17Barray(_n, _x, _y, _z, _Bx, _By, _Bz, _nP, _Plen, _Params)
