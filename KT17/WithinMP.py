92e38de38c421340ecac52fa0e85a7b6eef9fcbd
mattkjames7 <mattkjames7@gmail.com>
2022-03-21 22:51:44 +0000

    updated WithinMP code

--- KT17/WithinMP.py
+++ KT17/WithinMP.py
@@ -1,8 +1,6 @@
 import numpy as np
 from ._CFunctions import _CWithinMP
 
-###### File created automatically using PopulateCtypes ######
-
 def WithinMP(x, y, z, Rsm):
 	'''
 	Tests whether a position in MSM coordinates is within the 
@@ -10,19 +8,37 @@ def WithinMP(x, y, z, Rsm):
 	
 	Inputs
 	======
-	x,y,z: Scalars containing a position in MSM coordinates.
-	Rsm: Subsolar distance of the magnetopause in R_m.
-	
+	x : float
+		Scalar or array of x MSM coordinate(s) (Rm)
+	y : float
+		Scalar or array of y MSM coordinate(s) (Rm)
+	z : float
+		Scalar or array of z MSM coordinate(s) (Rm)
+	Rsm : float
+		Subsolar magnetopause standoff distance (Rm).
+			
 	Returns
 	=======
-	Boolean - True if within the magnetopause.
-	
+	res : bool
+		True if within the magnetopause.
+
 	'''
 	#Convert input variables to appropriate numpy dtype:
-	_x = np.float64(x)
-	_y = np.float64(y)
-	_z = np.float64(z)
-	_Rsm = np.float64(Rsm)
-	res = _CWithinMP(_x, _y, _z, _Rsm)
+	if np.size(x) == 1:
+		_x = np.float64(x)
+		_y = np.float64(y)
+		_z = np.float64(z)
+		_Rsm = np.float64(Rsm)
+		res = _CWithinMP(_x, _y, _z, _Rsm)
+	else:
+		n = np.size(x)
+		res = np.zeros(n,dtype='bool')
+		rsm = np.zeros(n,dtype='float64') + Rsm
+		for i in range(0,n):
+			_x = np.float64(x[i])
+			_y = np.float64(y[i])
+			_z = np.float64(z[i])
+			_Rsm = np.float64(rsm[i])
+			res[i] = _CWithinMP(_x, _y, _z, _Rsm)			
 
 	return res
